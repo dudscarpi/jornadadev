969b5ddbc2c2de77b56cff302589fbb3781d7ac8
Eduara Oliveira <Eduara.Oliveira@sintesesolucoes.com.br>
2023-06-21 13:48:49 -0300

    aula 3

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter";
 import "./video.css";
 
 function Video() {
@@ -19,7 +20,6 @@ function Video() {
 
   return (
     <div className="video">
-      Eu sou um componente Video
       <video
         className="video__player"
         ref={videoRef}
@@ -27,6 +27,8 @@ function Video() {
         loop
         src="https://firebasestorage.googleapis.com/v0/b/jornada-b5e9d.appspot.com/o/brecker2.mp4?alt=media&token=9aa979e9-0907-4093-abd5-a71efe4fc946"
       ></video>
+      {/*/ side bar*/}
+      <VideoFooter />
     </div>
   );
 }
